35214829e7c69b9613441d6f180b822f068cd3e6
Sebastian Kopf <seb.kopf@gmail.com>
2016-02-29 13:01:37 -0700

    code base initial commit

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' @name isocyclr
+#' @title Model isotope fractionation in biogeochemical cycles and pathways
+#' @description This package facilitates the modeling of isotopic effects through biogeochemical pathways and cycles including non-steady state and steady-state solutions.
+#' @author Sebastian Kopf
+#' @docType package
+#' @import dplyr lazyeval
+#' @importFrom tidyr gather gather_ extract extract_ spread spread_
+#' @include code.R
+NULL
