69ae7be2204589b75e15787a83499254a16be99d
Sebastian Kopf <seb.kopf@gmail.com>
2016-03-15 23:19:42 -0600

    support parallel reactions

--- R/plotting.R
+++ R/plotting.R
@@ -13,25 +13,40 @@ generate_reaction_diagram <- function(ip) {
 
   # determine y locations for components
   components_xy <-
-    general_x %>% select(isotope, component, x) %>%
-    distinct() %>% group_by(isotope, x) %>%
+    general_x %>%
+    select(isotope, component, x) %>%
+    distinct() %>% arrange(component) %>%
+    group_by(isotope, x) %>%
     mutate(y = seq(-length(x)+1, length(x)-1, length.out = length(x))) %>%
     ungroup()
 
-  # determin reaction lines
+  # determin reaction lines (with kinked lines but not accounting for parallel reactions)
+  # rxns_xy <-
+  #   general_x %>% left_join(components_xy %>% select(-x), by = c("component", "isotope")) %>%
+  #   mutate(
+  #     xstart = ifelse(comp_stoic > 0, x, x - 0.5),
+  #     xend = ifelse(comp_stoic > 0, x + 0.5, x),
+  #     ystart = ifelse(comp_stoic > 0, y, 0),
+  #     yend = ifelse(comp_stoic > 0, 0, y)
+  #   )
+
+  # direct lines but acounting for parallel reactions
+  rxn_components_xy <-
+    left_join(
+      general_x %>% select(reaction, component, isotope, comp_stoic, x),
+      components_xy,
+      by = c("component", "isotope", "x"))
   rxns_xy <-
-    general_x %>% left_join(components_xy %>% select(-x), by = c("component", "isotope")) %>%
-    mutate(
-      xstart = ifelse(comp_stoic > 0, x, x - 0.5),
-      xend = ifelse(comp_stoic > 0, x + 0.5, x),
-      ystart = ifelse(comp_stoic > 0, y, 0),
-      yend = ifelse(comp_stoic > 0, 0, y)
+    inner_join(
+      rxn_components_xy %>% filter(comp_stoic > 0) %>% rename(xstart = x, ystart = y),
+      rxn_components_xy %>% filter(comp_stoic < 0) %>% rename(xend = x, yend = y),
+      by = c("reaction", "isotope")
     )
 
   # plot everything
   ggplot() +
     geom_segment(
-      data = rxns_xy, size = 1, alpha = 0.5,
+      data = rxns_xy, size = 1.5, alpha = 0.8,
       #arrow = arrow(length = unit(0.03, "npc")),
       map = aes(x=xstart, y=ystart,
                 xend=xend, yend=yend,
