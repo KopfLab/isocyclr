a2d955dbccb72636f6c2d5bd45f2d9b787122433
Sebastian Kopf <seb.kopf@gmail.com>
2016-03-15 22:43:53 -0600

    check isotope names

--- R/isotope.R
+++ R/isotope.R
@@ -2,6 +2,7 @@
 #' @export
 add_isotope <- function(ip, name) {
   if (!is(ip, "isopath")) stop ("isotope can only be added to an isopath")
+  if (!grepl("^\\w+$", name)) stop("only alphanumeric isotope names allowed: ", name)
   ip$isotopes[[name]] <- list()
 
   return(ip)
