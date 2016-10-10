ac07324c8228e0606d34762a0e2163aa0818918e
Sebastian Kopf <seb.kopf@gmail.com>
2016-10-10 00:29:32 -0600

    add_arrows option in plotting and x alignment

--- R/plotting.R
+++ R/plotting.R
@@ -3,8 +3,9 @@
 #' plot reaction diagram
 #'
 #' @param y_data data frame with component and y columns to be used for plotting. If missing, arranges diagram like reaction scheme.
+#' @param arrows whether to generate arrows for reaction diagram
 #' @export
-generate_reaction_diagram <- function(ip, y_data = NULL) {
+generate_reaction_diagram <- function(ip, y_data = NULL, add_arrows = FALSE) {
   if (!is(ip, "isopath")) stop ("can only generate diagram for isopath", call. = FALSE)
 
   if (length(ip$reactions) == 0 || length(ip$components) == 0)
@@ -44,6 +45,7 @@ generate_reaction_diagram <- function(ip, y_data = NULL) {
       filter(!is.na(y))
   }
 
+
   # grouping parameters (relevant if data_y provided):
   grouping <- setdiff(names(components_xy), c("isotope", "component", "variable", "x", "y"))
 
@@ -53,7 +55,16 @@ generate_reaction_diagram <- function(ip, y_data = NULL) {
       general_x %>% select(reaction, component, isotope, comp_stoic, x),
       components_xy,
       by = c("component", "isotope", "x")) %>%
-    filter(!is.na(y))
+    filter(!is.na(y)) %>%
+    # move reaction components horizontally to largest x (i.e. the last occurence of a component)
+    group_by(component) %>%
+    mutate(max_x = max(x)) %>%
+    group_by(component, isotope) %>%
+    mutate(y = y[x == max_x][1], x = max_x) %>%
+    ungroup()
+
+  # arrow location (% along the line)
+  location <- 0.75
 
   rxns_xy <-
     inner_join(
@@ -61,6 +72,7 @@ generate_reaction_diagram <- function(ip, y_data = NULL) {
       rxn_components_xy %>% filter(comp_stoic < 0) %>% rename(xend = x, yend = y),
       by = c("reaction", "isotope", grouping)
     ) %>%
+    # ofset for parallel lines
     group_by(isotope, xstart, ystart, xend, yend) %>%
     arrange(reaction) %>%
     # offset in y direction depending on multi-line reactions
@@ -69,6 +81,12 @@ generate_reaction_diagram <- function(ip, y_data = NULL) {
     mutate(
       ystart = ystart + y_offset,
       yend = yend + y_offset
+    ) %>%
+    # arrows
+    group_by(isotope, reaction, component.x, component.y) %>%
+    mutate(
+      xarrow = xend + (xstart - xend) * location,
+      yarrow = yend + (ystart - yend) * location
     )
 
   # theme
@@ -89,15 +107,34 @@ generate_reaction_diagram <- function(ip, y_data = NULL) {
   }
 
   # plot everything
-  ggplot() +
-    geom_segment(
-      data = rxns_xy, size = 1.5,
-      #arrow = arrow(length = unit(0.03, "npc")),
-      map = aes(x=xstart, y=ystart,
-                xend=xend, yend=yend,
-                color = reaction)) +
+  p <- ggplot()
+
+  if (add_arrows) {
+    p <- p +
+      geom_segment(
+        data = rxns_xy, size = 1.5,
+        map = aes(x=xarrow, y=yarrow,
+                  xend=xstart, yend=ystart,
+                  color = reaction)) +
+
+      geom_segment(
+        data = rxns_xy, size = 1.5,
+        arrow = arrow(length = unit(0.05, "npc")),
+        map = aes(x=xend, y=yend,
+                  xend=xarrow, yend=yarrow,
+                  color = reaction))
+  } else {
+    p <- p +
+      geom_segment(
+        data = rxns_xy, size = 1.5,
+        map = aes(x=xend, y=yend,
+                  xend=xstart, yend=ystart,
+                  color = reaction))
+  }
+
+  p +
     geom_label(
-      data = components_xy, hjust = 0.5,
+      data = rxn_components_xy %>% select(component, isotope, x, y, variable) %>% unique(), hjust = 0.5,
       map = aes(x, y, label = component,
                 fill = ifelse(variable, "variable", "fixed"))) +
     facet_grid(isotope~.) +
